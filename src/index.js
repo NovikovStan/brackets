b96b37b9a8e1d23b0a0f7689105e3d88b3348937
Novikov Stanislav <novikov.stan@mail.ru>
2020-03-19 05:12:22 +0300

    feat brackets task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let outputArray = [str[0]];
+    let checker = true;
+    for (let i = 1; i < str.length; i++) {
+        if (checker == false) break;
+        else {
+            for (let j = 0; j < bracketsConfig.length; j++) {
+                if (str[i] == bracketsConfig[j][0]) {
+                    if (
+                        bracketsConfig[j][0] == bracketsConfig[j][1] &&
+                        outputArray[outputArray.length - 1] ==
+                            bracketsConfig[j][0]
+                    )
+                        outputArray.pop();
+                    else outputArray.push(str[i]);
+                    checker = true;
+                    break;
+                } else if (str[i] == bracketsConfig[j][1]) {
+                    if (
+                        outputArray[outputArray.length - 1] ==
+                        bracketsConfig[j][0]
+                    ) {
+                        outputArray.pop();
+                        checker = true;
+                        break;
+                    } else {
+                        checker = false;
+                        break;
+                    }
+                } else {
+                    checker = false;
+                }
+            }
+        }
+    }
+    if (checker && !outputArray.length) return true;
+    else return false;
+    // your solution
+};
